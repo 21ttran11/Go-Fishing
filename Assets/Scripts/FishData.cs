8d889aa1a07c2219c666c6b2ec284b8cdcca8e30
21ttran11 <58841978+21ttran11@users.noreply.github.com>
2025-04-16 23:42:47 -0700

    🐟

--- Assets/Scripts/FishData.cs
+++ Assets/Scripts/FishData.cs
@@ -5,7 +5,9 @@ public class FishData : ScriptableObject
 {
     public string fishId;         
     public string fishName;       
-    public Sprite fishSprite;     
+    public Sprite fishSprite;
+    public Sprite nameTag;
+    public Sprite unknownSprite;
     public string description;    
     public float rarity;          
 }
